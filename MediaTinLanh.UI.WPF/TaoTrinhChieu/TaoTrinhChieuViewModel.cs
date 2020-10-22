61b9aa2dce042f119307c827be47f2b6b0c001ee
Khoa Le Minh <p3t3r276@gmail.com>
2020-10-22 22:35:25 +0700

    Add view model TaoTrinhChieu

---
+++ MediaTinLanh.UI.WPF/TaoTrinhChieu/TaoTrinhChieuViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace MediaTinLanh.UI.WPF
+{
+    public class SlideData
+    {
+        public string NoiDung { get; set; }
+        public int ViTri { get; set; }
+    }
+
+    public class TaoTrinhChieuViewModel : INotifyPropertyChanged
+    {
+        private string _noiDungNhap;
+        private List<SlideData> slides { get; set; }
+
+        public string NoiDungNhap
+        {
+            get { return _noiDungNhap; }
+            set
+            {
+                _noiDungNhap = value;
+                OnPropertyChanged(nameof(NoiDungNhap));
+            }
+        }
+
+        public List<SlideData> Slides
+        {
+            get { return slides; }
+            set
+            {
+                slides = value;
+                OnPropertyChanged(nameof(Slides));
+            }
+        }
+
+        public void NoiDungToSlide()
+        {
+            string[] stringSlits = _noiDungNhap.Split(new[] { "\n\n" }, System.StringSplitOptions.None);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- MediaTinLanh.UI.WPF/TaoTrinhChieu/TuTaoTrinhChieuUC.xaml.cs
+++ MediaTinLanh.UI.WPF/TaoTrinhChieu/TuTaoTrinhChieuUC.xaml.cs
@@ -1,28 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace MediaTinLanh.UI.WPF.TaoTrinhChieu
+﻿namespace MediaTinLanh.UI.WPF.TaoTrinhChieu
 {
     /// <summary>
     /// Interaction logic for TuTaoTrinhChieuUC.xaml
     /// </summary>
-    public partial class TuTaoTrinhChieuUC : UserControl
+    public partial class TuTaoTrinhChieuUC : System.Windows.Controls.UserControl
     {
+        TaoTrinhChieuViewModel viewModel = new TaoTrinhChieuViewModel();
         public TuTaoTrinhChieuUC()
         {
             InitializeComponent();
+            this.DataContext = viewModel;
+        }
+
+        private void NoiDungNhap_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                var lastword = viewModel.NoiDungNhap.Substring(viewModel.NoiDungNhap.Length - 1);
+                var loiBaiHatArray = viewModel.NoiDungNhap.Split(new[] { "\n\n" }, System.StringSplitOptions.None);
+            }
         }
     }
 }
